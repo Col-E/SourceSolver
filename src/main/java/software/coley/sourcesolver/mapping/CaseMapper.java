89665c9b784b4277df6317565ca96e4448ae0d75
Matt <mcoley2@gmu.edu>
2024-12-27 05:44:04 -0500

    Fix NPE with switches using yield model

--- src/main/java/software/coley/sourcesolver/mapping/CaseMapper.java
+++ src/main/java/software/coley/sourcesolver/mapping/CaseMapper.java
@@ -1,12 +1,17 @@
 package software.coley.sourcesolver.mapping;
 
 import com.sun.source.tree.CaseTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.StatementTree;
+import com.sun.source.tree.Tree;
 import com.sun.tools.javac.tree.EndPosTable;
 import software.coley.sourcesolver.model.AbstractExpressionModel;
 import software.coley.sourcesolver.model.AbstractStatementModel;
 import software.coley.sourcesolver.model.CaseModel;
+import software.coley.sourcesolver.model.Model;
 
 import javax.annotation.Nonnull;
+import java.util.Collections;
 import java.util.List;
 
 import static software.coley.sourcesolver.util.Range.extractRange;
@@ -18,9 +23,19 @@ public class CaseMapper implements Mapper<CaseModel, CaseTree> {
 		List<AbstractExpressionModel> expressions = tree.getExpressions().stream()
 				.map(c -> context.map(ExpressionMapper.class, c))
 				.toList();
-		List<AbstractStatementModel> statements = tree.getStatements().stream()
+		Model body = tree.getBody() == null ? null : mapBody(context, tree.getBody());
+		List<AbstractStatementModel> statements = tree.getStatements() == null ? Collections.emptyList() : tree.getStatements().stream()
 				.map(c -> context.map(StatementMapper.class, c))
 				.toList();
-		return new CaseModel(extractRange(table, tree), expressions, statements);
+		return new CaseModel(extractRange(table, tree), expressions, statements, body);
+	}
+
+	@Nonnull
+	private Model mapBody(@Nonnull MappingContext context, @Nonnull Tree tree) {
+		if (tree instanceof ExpressionTree expression)
+			return context.map(ExpressionMapper.class, expression);
+		else if (tree instanceof StatementTree statementMapper)
+			return context.map(StatementMapper.class, statementMapper);
+		throw new IllegalStateException("Case body was not an expression or statement");
 	}
 }
--- src/main/java/software/coley/sourcesolver/model/CaseModel.java
+++ src/main/java/software/coley/sourcesolver/model/CaseModel.java
@@ -3,7 +3,9 @@ package software.coley.sourcesolver.model;
 import software.coley.sourcesolver.util.Range;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static software.coley.sourcesolver.model.ChildSupplier.of;
@@ -11,11 +13,14 @@ import static software.coley.sourcesolver.model.ChildSupplier.of;
 public class CaseModel extends AbstractModel {
 	private final List<AbstractExpressionModel> expressions; // case <expression>
 	private final List<AbstractStatementModel> statements; // body of cases
+	private final Model body; // body of cases
 
-	public CaseModel(@Nonnull Range range, @Nonnull List<AbstractExpressionModel> expressions, @Nonnull List<AbstractStatementModel> statements) {
-		super(range, of(expressions), of(statements));
+	public CaseModel(@Nonnull Range range, @Nonnull List<AbstractExpressionModel> expressions,
+	                 @Nonnull List<AbstractStatementModel> statements, @Nullable Model body) {
+		super(range, of(expressions), of(statements), of(body));
 		this.expressions = expressions;
 		this.statements = statements;
+		this.body = body;
 	}
 
 	@Nonnull
@@ -28,6 +33,11 @@ public class CaseModel extends AbstractModel {
 		return statements;
 	}
 
+	@Nullable
+	public Model getBody() {
+		return body;
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
@@ -35,22 +45,25 @@ public class CaseModel extends AbstractModel {
 
 		CaseModel caseModel = (CaseModel) o;
 
-		if (!getRange().equals(caseModel.getRange())) return false;
 		if (!expressions.equals(caseModel.expressions)) return false;
-		return statements.equals(caseModel.statements);
+		if (!statements.equals(caseModel.statements)) return false;
+		return Objects.equals(body, caseModel.body);
 	}
 
 	@Override
 	public int hashCode() {
-		int result = getRange().hashCode();
-		result = 31 * result + expressions.hashCode();
+		int result = expressions.hashCode();
 		result = 31 * result + statements.hashCode();
+		result = 31 * result + (body != null ? body.hashCode() : 0);
 		return result;
 	}
 
 	@Override
 	public String toString() {
-		return "case " + expressions.stream().map(Object::toString).collect(Collectors.joining(", ")) + ":\n    "
+		String expressions = this.expressions.stream().map(Object::toString).collect(Collectors.joining(", "));
+		if (body != null)
+			return "case " + expressions + " -> " + body;
+		return "case " + expressions + ":\n    "
 				+ statements.stream().map(Object::toString).collect(Collectors.joining("\n    "));
 	}
 }
